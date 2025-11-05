176ec0a13c9afaedd7bb54d89f2687966d813a99
andreipapp <169454559+andreipapp@users.noreply.github.com>
2025-11-05 15:18:10 +0200

    removing placeholder pseudo

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+const input = document.getElementById('input');
+const btn = document.getElementById('btn');
+input.value = 20;

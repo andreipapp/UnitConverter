e4517c7b252c62ec2c841a8cbd29d1ddfcfae161
andreipapp <169454559+andreipapp@users.noreply.github.com>
2025-11-05 15:33:09 +0200

    small changes + js - project done

--- index.js
+++ index.js
@@ -5,4 +5,23 @@
 */
 const input = document.getElementById('input');
 const btn = document.getElementById('btn');
+const length = document.getElementById('length');
+const volume = document.getElementById('volume');
+const mass = document.getElementById('mass');
+
 input.value = 20;
+
+btn.addEventListener('click', convert);
+
+function convert() {
+  let value = input.value;
+  length.textContent = `${value} meters = ${(value * 3.281).toFixed(
+    3
+  )} feet | ${value} feet = ${(value / 3.281).toFixed(3)} meters`;
+  volume.textContent = `${value} liters = ${(value * 0.264).toFixed(
+    3
+  )} gallons | ${value} gallons = ${(value / 0.264).toFixed(3)} liters`;
+  mass.textContent = `${value} kilos = ${(value * 2.204).toFixed(
+    3
+  )} pounds | ${value} pounds = ${(value / 2.204).toFixed(3)} kilos`;
+}
